256bb59d905d34ccf4e7f494edb8e0c291cc7e3e
anubhavsahoo <anubhav.sahoo@gmail.com>
2016-01-16 13:08:04 +0530

    GORL init

--- generateOpsReportLite.js
+++ generateOpsReportLite.js
@@ -5,6 +5,12 @@ function importScript (sSrc, fOnload) {
   if (fOnload) { oScript.onload = fOnload; }
   document.currentScript.parentNode.insertBefore(oScript, document.currentScript);
   oScript.src = sSrc;
+};
+function onScriptLoad(){
+  LOAD_COUNTER++;
+  if(LOAD_COUNTER == 2) init();
 }
-
-importScript()
+var LOAD_COUNT = 2;
+var LOAD_COUNTER = 0;
+importScript('https://code.jquery.com/jquery-1.12.0.min.js', onScriptLoad);
+importScript('https://code.jquery.com/jquery-1.12.0.min.js', onScriptLoad);
